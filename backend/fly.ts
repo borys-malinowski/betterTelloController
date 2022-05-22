9d0db030fb84bdf32c32f6da9a7481c686b4e342
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2022-05-22 21:49:53 +0200

    Add drone state

--- backend/fly.ts
+++ backend/fly.ts
@@ -2,14 +2,23 @@ import dgram from "dgram";
 import express from 'express';
 import  { Server } from "socket.io";
 
-
 const PORT = 8889;
+const STATEPORT = 8890;
 const HOST = "192.168.10.1";
+const HOSTPORT = "0.0.0.0";
 
 const tello = dgram.createSocket("udp4");
 tello.bind(PORT);
 
+const telloState = dgram.createSocket("udp4");
+telloState.bind(STATEPORT, HOSTPORT);
+
 tello.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`);
+  tello.close();
+});
+
+telloState.on('error', (err) => {
   console.log(`server error:\n${err.stack}`);
   tello.close();
 });
@@ -18,18 +27,22 @@ tello.on("message", (message) => {
   console.log(`drone tell: ${message}`);
 });
 
+telloState.on("message", state  => {
+  console.log(`drone position: ${state}`);
+});
+
 function errorHandler(error: Error | null) {
   if (error) {
     console.log("ERROR");
     console.log(error);
   }
-}
+};
 
 const app = express();
 
 const server = app.listen(6767, () => {
   console.log('socket io server ip and running');
-})
+});
 
 const io = new Server(server);
 
