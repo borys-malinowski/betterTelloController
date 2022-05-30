83ee030f6e55e30c47d06d86c77ebd53881183af
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2022-05-30 21:38:02 +0200

    Update and add types

--- backend/fly.ts
+++ backend/fly.ts
@@ -1,61 +1,65 @@
-import dgram from "dgram";
-import express from "express";
-import { Server } from "socket.io";
+import dgram, { Socket as DgramSocket } from "dgram";
+import express, { Express } from "express";
+import { Server as HTTPServer } from "http";
+import { Server, Socket } from "socket.io";
+import DroneStateType from "DroneStateType"
+import errorHandler from "errorHandler"
 
-const PORT = 8889;
-const STATEPORT = 8890;
-const HOST = "192.168.10.1";
-const HOSTPORT = "0.0.0.0";
 
-function errorHandler(error: Error | null) {
-  if (error) {
-    console.log("ERROR");
-    console.log(error);
-  }
-}
+type ParsedState = { [key in keyof DroneStateType]: DroneStateType };
+type ParsedStateAsTuple = [keyof DroneStateType, DroneStateType];
 
-const tello = dgram.createSocket("udp4");
-tello.bind(PORT);
+const PORT: number = 8889;
+const STATEPORT: number = 8890;
+const HOST: string = "192.168.10.1";
+const HOSTPORT: string = "0.0.0.0";
 
-const telloState = dgram.createSocket("udp4");
+const telloSocket: DgramSocket = dgram.createSocket("udp4");
+telloSocket.bind(PORT);
+
+const telloState: DgramSocket = dgram.createSocket("udp4");
 telloState.bind(STATEPORT, HOSTPORT);
 
-const app = express();
+const app: Express = express();
 
-const server = app.listen(6767, () => {
+const server: HTTPServer = app.listen(6767, (): void => {
   console.log("socket io server ip and running");
 });
 
-const io = new Server(server);
+const io: Server = new Server(server);
 
-io.on("connection", (socket) => {
-  tello.on("error", (err) => {
-    console.log(`server error:\n${err.stack}`);
-    tello.close();
+io.on("connection", (socket: Socket): void => {
+  telloSocket.on("error", ({ message }: Error): void => {
+    console.error(`server error:\n${message}`);
+    telloSocket.close();
   });
 
-  tello.on("message", (message) => {
+  telloSocket.on("message", (message: Buffer): void => {
     console.log(`drone tell: ${message}`);
   });
 
-  telloState.on("error", (err) => {
-    console.log(`server error:\n${err.stack}`);
-    tello.close();
+  telloState.on("error", ({ message }: Error): void => {
+    console.log(`server error:\n${message}`);
+    telloSocket.close();
   });
-  telloState.on("message", (state) => {
-    const parseState = state.toString();
-    const parsedState = parseState.split(";").map((x) => {
-      const [key, value] = x.split(":");
-      return {[key]: value}
-    });
-    const fixedParsedState = Object.assign({}, ...parsedState)
+  telloState.on("message", (state: Buffer): void => {
+    const parseState: string = state.toString();
+    const parsedState: ParsedState[] = parseState
+      .split(";")
+      .map((x): ParsedState => {
+        const [key, value]: ParsedStateAsTuple = x.split(
+          ":",
+        ) as ParsedStateAsTuple;
+        return { [key]: value } as ParsedState;
+      });
+    const fixedParsedState: DroneStateType = Object.assign({}, ...parsedState);
     console.log(`drone position: ${fixedParsedState}`);
     io.emit("dronestate", fixedParsedState);
   });
-  socket.on("command", (command) => {
+  socket.on("command", (command: string): void => {
     console.log(`Command sent from browser ${command.length}, ${command}`);
     try {
-      tello.send(command, 0, command.length, PORT, HOST, errorHandler);
+      telloSocket.send(command, 0, command.length, PORT, HOST, errorHandler);
     } catch (error) {
       console.error(error);
     }
--- telloapp/src/hooks/droneState.tsx
+++ telloapp/src/hooks/droneState.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import {clearSocket} from '../socket'
+import {DroneStateType} from "../../../backend/fly"
 
 function useDroneState() {
     const [telloState, setTelloState] = useState<DroneStateType | null>(null);
@@ -10,25 +11,6 @@ function useDroneState() {
 
     return telloState
 };
-
-type DroneStateType = {
-    pitch: number,
-    roll: number,
-    yaw: number,
-    vgx: number,
-    vgy: number,
-    vgz: number,
-    templ: number,
-    temph: number,
-    tof: number,
-    h: number,
-    bat: number,
-    baro: number,
-    time: number,
-    agx: number,
-    agy: number,
-    agz: number,
-}
 
 const DroneState =() => {
     const droneState = useDroneState();
