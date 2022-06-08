058eb783e30cd55fb19a9fd34a17a2f2d88d51a2
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2022-06-08 20:46:08 +0200

    Move bot to main file

--- backend/src/fly.ts
+++ backend/src/fly.ts
@@ -8,6 +8,41 @@ import ParsedState from "./types/typeParsedState";
 import ParsedStateAsTuple from "./types/typeParsedStateAsTuple";
 import { HOST, PORT } from "./constants";
 import telloStream from "./sockets/telloStream";
+import {Client, Message} from 'discord.js';
+import formatMessageContent from "./discordBot/formatMessageContent";
+import getTakeoff from "./discordBot/getTakeoff";
+import {PREFIX} from "./discordBot/formatMessageContent"
+import { config } from "dotenv";
+import Options from "./discordBot/typeOptions";
+import getStart from "./discordBot/getStart";
+
+config()
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+const getCommandMapper = async (message: Message<boolean>) => {
+    const [cmdName] = formatMessageContent(message.content)
+    const options: Options= {
+        takeoff: async () => {
+            await getTakeoff(message);
+        },
+        start: async () => {
+            await getStart(message);
+        }
+    }
+    await options[options.hasOwnProperty(cmdName) ? cmdName : "default"](message)
+};
+
+client.on("messageCreate", async (message) => {
+    if (message.author.bot === true) {
+      return;
+    }
+    if (message.content.startsWith(PREFIX)) {
+      await getCommandMapper(message);
+    }
+  });
+  
+  client.login(process.env.DISCORD_BOT_TOKEN);
+
 
 io.on("connection", (socket: Socket): void => {
   telloSocket.on("error", ({ message }: Error): void => {
